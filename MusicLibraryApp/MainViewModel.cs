50f7793d1468acafaefe1af658d477b510fa83c8
Wen <8473652+wendyscarlet@users.noreply.github.com>
2018-06-26 15:08:46 -0700

    New Project MusicLibrary Replace MyNotes
    MyNotesApps have been replaced with a new Project Named MusicLibraryApp

---
+++ MusicLibraryApp/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MusicLibraryApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibraryApp
+{
+    class MainViewModel
+    {
+
+        static public  List<Song> SongsList { get; set; }
+
+
+        public void AddSong(Song s) {
+
+
+        }
+    }
+}
---
+++ MusicLibraryApp/Model/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MusicLibraryApp
+{
+    class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+       // public Bitmap CoverImage { get; set; }     
+        public string AudioFilePath { get; set; }
+
+    }
+}
