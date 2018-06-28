6c8a5fb635a76ca1839986e537a57fede6f5a8e3
surabivishnupriya <35674260+surabivishnupriya@users.noreply.github.com>
2018-06-27 21:06:53 -0700

    1.Capturing data and saving into file 2.Added file picker for song browse option 3.Added the textplace holders for title and artist

--- MusicLibraryApp/AppDialogs/AddSongContentDialog.xaml.cs
+++ MusicLibraryApp/AppDialogs/AddSongContentDialog.xaml.cs
@@ -25,15 +25,36 @@ namespace MusicLibraryApp
         }
         private void ContentDialog_AddButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+            var song = new Model.Song
+            {
+                Title = Title.Text,
+                Artist = Artist.Text,
+                Album = "Pop",
+                AudioFilePath = Song.Text
+            };
+            FileHelper.WriteSongToFileAsync(song);
+
         }
 
         private void ContentDialog_CancelButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
         }
-        private void ContentDialog_TakeImage_Clicked(object sender, RoutedEventArgs e)
+       
+
+        private async void ContentDialog_TakeImage_Clicked(object sender, RoutedEventArgs e)
         {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
 
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.List;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.MusicLibrary;
+            picker.FileTypeFilter.Add(".mp3");
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                this.Song.Text =  file.Path;
+            }
         }
-
+        
     }
 }
