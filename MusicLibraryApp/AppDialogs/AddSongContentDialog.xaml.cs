a4b5b0a450910ae33d081b5878d96d6dc7283647
Wen <8473652+wendyscarlet@users.noreply.github.com>
2018-07-03 15:12:23 -0700

    Revert "Revert "Merge branch 'master' of https://kalacademy.visualstudio.com/SoftwareDevC1Team5/_git/SoftwareDevC1Team5""
    This reverts commit 57f9323cddbe4f4244b7f947743c2679d4589e14.

--- MusicLibraryApp/AppDialogs/AddSongContentDialog.xaml.cs
+++ MusicLibraryApp/AppDialogs/AddSongContentDialog.xaml.cs
@@ -61,12 +61,8 @@ namespace MusicLibraryApp.AppDialogs
 
         private void Song_TextChanged(object sender, TextChangedEventArgs e)
         {
-<<<<<<< HEAD
             if(songFile!=null)
             IsPrimaryButtonEnabled = true;
-=======
-
->>>>>>> parent of 29f76cf... Merge branch 'master' of https://kalacademy.visualstudio.com/SoftwareDevC1Team5/_git/SoftwareDevC1Team5
         }
     }
 }
